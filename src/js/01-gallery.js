a7c77b1353a17c38c6fd0579de2dd7e680b0c490
ViktoriaTorianik <122830538+ViktoriaTorianik@users.noreply.github.com>
2023-06-04 23:57:38 +0300

    first commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+console.log(SimpleLightbox);
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+const galleryEl = document.querySelector(".gallery");
+galleryEl.style.listStyle = "none";
+
+const pictureCards = createPictureGalery(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', pictureCards);
+
+function createPictureGalery (item){
+return galleryItems.map(({preview, original, description}) =>{
+    return`
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `  
+}).join("");
+}
+var lightbox = new SimpleLightbox('.gallery a', 
+{captionsData: "alt", captionDelay: "250"})
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle  from "lodash.throttle";
 
+const formEl = document.querySelector(".feedback-form")
+const emailEl = document.querySelector('.feedback-form input');
+const messageEl = document.querySelector('.feedback-form textarea')
+
+const STORAGE_KEY = "feedback-form-state"
+
+
+formEl.addEventListener('submit', onFormSubmit)
+formEl.addEventListener('input',throttle(onFormInput, 500));
+const formData = {};
+returnFeedback()
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  console.log(e.target.value);
+}
+
+function onFormSubmit(e){
+console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));  
+e.preventDefault();
+
+if(e.target.elements.email.value === "" || e.target.elements.message.value === ""){
+   return alert('всі поля повинні бути заповнені')
+} 
+
+e.target.reset()
+
+localStorage.removeItem(STORAGE_KEY)
+
+}
+
+function returnFeedback(){
+    const preMassage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+ 
+    if (preMassage) {
+        emailEl.value = preMassage.email;
+        messageEl.value = preMassage.message;
+    }
+    console.log();
+}
